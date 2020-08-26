25ceff9324408ac42048914eb25e99ff7daa41d6
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-08-26 17:06:58 -0400

    Severe Weather QA- populate geoids by points-updates in the query

--- src/tasks/QA/severeWeather/populate_geoids_by_points/populate_geoids_cousubs_by_points.py
+++ src/tasks/QA/severeWeather/populate_geoids_by_points/populate_geoids_cousubs_by_points.py
@@ -6,29 +6,37 @@ EARLIEST_YEAR = 1996
 LATEST_YEAR = 2018
 fips = ["01", "02", "04", "05","06", "08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
         "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35","37", "38", "39", "40", "41", "42", "44", "45", "46",
-        "47", "48", "49", "50", "51", "53", "54", "55", "56"]
+        "47", "48", "49", "50", "51", "53", "54", "55", "56","72"]
 
 def calculate(cursor):
-    for fip in fips:
-        print('fip',fip)
-        geo_table_name = 'geo.tl_2017_' + fip + '_cousub'
-        sql="""
+    fip = sys.argv[1]
+    print(fip)
+    geo_table_name = 'geo.tl_2017_' + fip + '_cousub'
+    sql="""
         with t as (
-              SELECT begin_lat,begin_lon,geoid,cousub_geoid
+              SELECT event_id,begin_lat,begin_lon,geoid,cousub_geoid,tract_geoid,state_fips,year
               FROM severe_weather.details
               where begin_lat is not null and begin_lon is not null
               and state_fips = """+str(fip)+"""
               and year >= """+str(EARLIEST_YEAR)+"""
-              )
-        UPDATE severe_weather.details as b
-	    SET geoid= substring(a.geoid,1,5), cousub_geoid=a.geoid
-        FROM """+geo_table_name+""" as a,t
-        WHERE ST_CONTAINS(a.geom,ST_SetSRID(ST_Point(t.begin_lon,t.begin_lat), 4326))
-		and t.begin_lat = b.begin_lat
-		and t.begin_lon = b.begin_lon
-        and state_fips = """+str(fip)+"""
+              ),
+		s as (
+		SELECT t.event_id as event_id,
+			a.geoid as cousub_geoid,
+			substring(a.geoid,1,5) as geoid
+        FROM """+geo_table_name+""" as a
+        JOIN t on ST_CONTAINS(a.geom,ST_SetSRID(ST_Point(t.begin_lon,t.begin_lat), 4326))
+        WHERE t.state_fips = """+str(fip)+"""
+		and t.year >="""+str(EARLIEST_YEAR)+""")
+		
+		UPDATE severe_weather.details as b
+		SET cousub_geoid = s.cousub_geoid,
+		geoid = substring(s.cousub_geoid,1,5)
+		FROM s
+		WHERE b.event_id = s.event_id
         """
-        cursor.execute(sql)
+    cursor.execute(sql)
+
 
 def main():
 
--- src/tasks/QA/severeWeather/populate_geoids_by_points/populate_tracts_geoids_by_points.py
+++ src/tasks/QA/severeWeather/populate_geoids_by_points/populate_tracts_geoids_by_points.py
@@ -1,4 +1,4 @@
-import argparse, csv, io, psycopg2,sys,pandas,numpy as np
+import argparse, csv, io, psycopg2,sys,pandas,numpy as np,sys
 
 import database_config
 
@@ -6,30 +6,36 @@ EARLIEST_YEAR = 1996
 LATEST_YEAR = 2018
 fips = ["01", "02", "04", "05","06", "08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
         "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35","36","37", "38", "39", "40", "41", "42", "44", "45", "46",
-        "47", "48", "49", "50", "51", "53", "54", "55", "56"]
-
+        "47", "48", "49", "50", "51", "53", "54", "55", "56","72"]
+new_fips =['81','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99']
+print(len(fips))
 
 def calculate(cursor):
-    for fip in fips:
-        print('fip',fip)
-        geo_table_name = 'geo.tl_2017_' + fip + '_tract'
-        sql="""
+    fip = sys.argv[1]
+    print(fip)
+    geo_table_name = 'geo.tl_2017_' + fip + '_tract'
+    sql="""
         with t as (
-              SELECT begin_lat,begin_lon,geoid,cousub_geoid,tract_geoid
+              SELECT event_id,begin_lat,begin_lon,geoid,cousub_geoid,tract_geoid,state_fips,year
               FROM severe_weather.details
               where begin_lat is not null and begin_lon is not null
               and state_fips = """+str(fip)+"""
               and year >= """+str(EARLIEST_YEAR)+"""
-              )
-        UPDATE severe_weather.details as b
-	    SET t.tract_geoid=a.geoid
-        FROM """+geo_table_name+""" as a,t
-        WHERE ST_CONTAINS(a.geom,ST_SetSRID(ST_Point(t.begin_lon,t.begin_lat), 4326))
-		and t.begin_lat = b.begin_lat
-		and t.begin_lon = b.begin_lon
-        and b.state_fips = """+str(fip)+"""
+              ),
+		s as (
+		SELECT t.event_id as event_id,a.geoid as geoid
+        FROM """+geo_table_name+""" as a
+        JOIN t on ST_CONTAINS(a.geom,ST_SetSRID(ST_Point(t.begin_lon,t.begin_lat), 4326))
+        WHERE t.state_fips = """+str(fip)+"""
+		and t.year >= """+str(EARLIEST_YEAR)+"""
+		)
+		
+		UPDATE severe_weather.details as b
+		SET tract_geoid = s.geoid
+		FROM s
+		WHERE b.event_id = s.event_id
         """
-        cursor.execute(sql)
+    cursor.execute(sql)
 
 def main():
 
